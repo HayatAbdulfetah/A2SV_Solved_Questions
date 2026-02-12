051fa409b5caf67c4c1af8d96e1ce96853cd91ee
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-02-12 11:05:12 +0300

    Refactor twoSum method to use nested loops

--- Two Sum.py
+++ Two Sum.py
@@ -1,13 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        left = 0
-        for i in range(1, len(nums)):
-            if nums[i] + nums[left] == target:
-                    return [left , i]
-            else:
-                left += 1
-                if left < i and nums[i] + nums[left] == target:
-                    return [left, i]
-            
-
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i , j]
+                
         return []
