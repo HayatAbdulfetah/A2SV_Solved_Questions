2c3af038a046427c4cc6ad944f92e615bb4f400a
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-03-08 23:55:50 +0300

    Create Determine if Two Strings Are Close.py

---
+++ Determine if Two Strings Are Close.py
@@ -0,0 +1,15 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+
+        hash1 = Counter(word1)
+        hash2 = Counter(word2)
+
+        if set(hash1.keys()) != set(hash2.keys()):
+            return False
+
+        if sorted(hash1.values()) != sorted(hash2.values()):
+            return False
+            
+        return True
