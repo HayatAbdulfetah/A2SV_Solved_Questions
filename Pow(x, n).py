2bb8151f78e3d8e80d9e91b7d05f209952fc8c8a
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-03-23 19:38:16 +0300

    Update Pow(x, n).py

--- Pow(x, n).py
+++ Pow(x, n).py
@@ -1,3 +1,11 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x **n
+        # you can simply return x **n
+        # or you can use recursion:
+        if n == 1:
+            return x
+        if n == 0:
+            return 1
+        x = pow(x,n-1)*x
+        
+        return x
