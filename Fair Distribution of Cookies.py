18aa4d406d6591e0e1fe3cc5f0c3e2f56748ccd5
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-03-31 21:43:06 +0300

    Implement cookie distribution solution using backtracking

---
+++ Fair Distribution of Cookies.py
@@ -0,0 +1,22 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        childs = [0] *k
+        
+        self.minUnfairness = float('inf')
+        
+        def backtrack(idx, max_so_far):
+            if max_so_far >= self.minUnfairness:
+                return 
+            if idx == len(cookies):
+                self.minUnfairness = min(self.minUnfairness , max(childs))
+                return 
+             
+            for i in range(k):
+                childs[i] += cookies[idx]
+                backtrack(idx+1, max(max_so_far, max(childs)))
+                childs[i] -= cookies[idx]
+                if childs[i] == 0:
+                    return
+        backtrack(0,0)
+
+        return self.minUnfairness
