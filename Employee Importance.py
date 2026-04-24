8d76e027e7991120516194ef800464da49178bc1
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-04-24 19:42:35 +0300

    Create Employee Importance.py

---
+++ Employee Importance.py
@@ -0,0 +1,23 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        mp = {emp.id: emp for emp in employees}
+        
+        def dfs(emp_id):
+            emp = mp[emp_id]
+            total = emp.importance
+            
+            for sub in emp.subordinates:
+                total += dfs(sub)
+            
+            return total
+            
+        return dfs(id)
